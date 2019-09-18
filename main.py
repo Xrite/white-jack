0e80cf7cb80af9f7167d0c23a13e8b05625c52a6
Alexandra Olemskaya <alexandra.olemskaya@gmail.com>
2019-09-18 17:32:36 +0300

    first windows

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
-from curses import wrapper
+import tkinter as tk
 
-def main(stdscr):
-    # Clear screen
-    stdscr.clear()
+def pass_onclick():
+    print("kek")
 
-    # This raises ZeroDivisionError when i == 10.
-    for i in range(0, 10):
-        v = i-10
-        stdscr.addstr(i, 0, '10 divided by {} is {}'.format(v, 10/v))
+def draw_onclick():
+    print("lol")
 
-    stdscr.refresh()
-    stdscr.getkey()
+def get_user_cards():
+    return "UC"
 
-wrapper(main)
+def get_dealer_cards():
+    return "DC"
+
+root = tk.Tk()
+
+root.geometry("800x600")
+
+user_cards = tk.Label(root, text=get_user_cards())
+dealer_cards = tk.Label(root, text=get_dealer_cards())
+
+pass_btn = tk.Button(root, text="PASS", width=10, height=5, bg="lightgrey", fg="black")
+draw_btn = tk.Button(root, text="DRAW", width=10, height=5, bg="lightgrey", fg="black")
+
+user_cards.pack()
+dealer_cards.pack()
+pass_btn.pack()
+draw_btn.pack()
+main_frame.pack()
+
+root.mainloop()
