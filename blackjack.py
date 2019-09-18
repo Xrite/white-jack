7925cf6816eca1f0a5567b906ccf618842c17a43
Your Name <you@example.com>
2019-09-18 17:23:33 +0300

    More

--- blackjack.py
+++ blackjack.py
@@ -12,13 +12,36 @@ class BlackjackGame:
         self.dealer = []
         self.deck = gen_deck()
         self.state = 'ongoing'
+        self.draw()
+        #if state != 'ongoing':
+        #    return
+        self.draw()
         
+    def get_card(self):
+        card = deck[-1]
+        deck.pop()
+        return card
         
     def draw(self):
-        
+        player.append(self.get_card())
+        self.reeval_state()
+        if self.state != 'ongoing':
+            return
+        self.ignore()
     
-    def fuck(self):
-        pass
+    def ignore(self):
+        if get_hand_value(self.dealer) < 17:
+            dealer.append(self.get_card())
+            self.reeval_state()
+        else:
+            player_value = get_hand_value(self.player)
+            dealer_value = get_hand_value(self.dealer)
+            if player_value < dealer_value:
+                self.state = 'dealerwin'
+            elif player_value == dealer_value:
+                self.state = 'draw'
+            else
+                self.state = 'playerwin'
     
     def get_dealer_hand(self):
         return self.dealer
@@ -48,3 +71,15 @@ class BlackjackGame:
             
     def get_hand_value(hand):
         return sum(map(get_card_value, hand))
+    
+    def reeval_state(self):
+        player_value = get_hand_value(self.player)
+        dealer_value = get_hand_value(self.dealer)
+        if player_value == 21:
+            self.state = 'playerwin'
+        elif player_value > 21:
+            self.state = 'dealerwin'
+        elif dealer_value > 21:
+            self.state = 'playerwin'
+        
+        
