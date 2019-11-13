43e95be6130c2d772bb84b8d47e5f7f35acfc4b5
Your Name <you@example.com>
2019-11-13 17:19:16 +0300

    Refactor

--- blackjack.py
+++ blackjack.py
@@ -10,42 +10,41 @@ class BlackjackGame:
         
     def new_game(self, num_players):
         self.num_players = num_players
-        self.hands = [[] for i in range(num_players)]
-        self.dealer = []
-        self.current_player
+        self.hands = [[] for i in range(num_players + 1)]
+        self.current_player = 0
         self.deck = gen_deck()
-        self.states = 'ongoing'
+        self.states = ['ongoing' for i in range(num_players + 1)]
         self.draw() # TODO
         self.draw()
         
-    def get_card(self):
+    def _get_card(self):
         card = self.deck[-1]
         self.deck.pop()
         return card
         
-    def draw(self):
-        self.player.append(self.get_card())
-        self.reeval_state()
-        if self.state != 'ongoing':
-            return
-        self.ignore()
+    def draw(self, player_num):
+        if player_num != self.current_player:
+            raise Exception()
+        card = self._get_card()
+        self.players[player_num].append(card)
+        self.reeval_state(player_num)
+        self.ignore(player_num)
+        return card
     
-    def ignore(self):
+    def ignore(self, player_num):
+        self.current_player += 1
+        if self.current_player == self.num_players:
+            self.dealer_move()
+            self.current_player = 0
+        # TODO
+                
+    def dealer_move(self):
         if get_hand_value(self.dealer) < dealer_bound:
-            self.dealer.append(self.get_card())
-            self.reeval_state()
-        else:
-            player_value = get_hand_value(self.player)
-            dealer_value = get_hand_value(self.dealer)
-            if player_value < dealer_value:
-                self.state = 'dealerwin'
-            elif player_value == dealer_value:
-                self.state = 'draw'
-            else:
-                self.state = 'playerwin'
+            self.dealer.append(self._get_card())
+            self.reeval_state(self.num_players)
     
-    def get_hands(self):
-        return (self.hands, self.dealer)
+    #def get_hands(self):
+        #return (self.hands, self.dealer)
     
     def get_current_player():
         return self.current_player
@@ -53,15 +52,12 @@ class BlackjackGame:
     def get_state(self, player_num):
         return self.states[player_num]
 
-    def reeval_state(self):
-        player_value = get_hand_value(self.player)
-        dealer_value = get_hand_value(self.dealer)
-        if player_value == 21:
-            self.state = 'playerwin'
-        elif player_value > 21:
-            self.state = 'dealerwin'
-        elif dealer_value > 21:
-            self.state = 'playerwin'
+    def _reeval_state(self, player_num):
+        player_values = get_hand_value(self.player)
+        if player_value == blackjack:
+            self.states[player_num] = 'blackjack'
+        elif player_value > blackjack:
+            self.states[player_num] = 'busted'
             
 def get_hand_value(hand):
     return sum(map(get_card_value, hand))
