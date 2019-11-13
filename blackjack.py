92c67e23ab01091d05bfd0694572be9d942c4e7b
Your Name <you@example.com>
2019-11-13 18:03:25 +0300

    UI

--- blackjack.py
+++ blackjack.py
@@ -2,50 +2,71 @@ from random import shuffle
 from card import gen_deck, Card
 from enum import Enum
 
+def get_hand_value(hand):
+    return sum(map(get_card_value, hand))
+
 class PlayerState(Enum):
     ongoing = 'ongoing'
     blackjack = 'blackjack'
     busted = 'busted'
 
+class GameState(Enum):
+    dealer_busted = 'dealer_busted'
+    all_busted = 'all_busted'
+    ongoing = 'ongoing'
+    one_winner = 'one_winner'
+
 class BlackjackGame:
     blackjack = 21
     dealer_bound = 17
     
-    def __init__(self):
-        self.new_game()
-        
-    def new_game(self, num_players):
+    def __init__(self, num_players, callback):
         self.num_players = num_players
         self.hands = [[] for i in range(num_players + 1)]
         self.current_player = 0
         self.deck = gen_deck()
         self.states = [PlayerState.ongoing for i in range(num_players + 1)]
-        self.draw() # TODO
-        self.draw()
+        self.callback = callback
+        #self.draw() # TODO
+        #self.draw()
         
     def _get_card(self):
         card = self.deck[-1]
         self.deck.pop()
         return card
+    
+    def _dispatch_callback(self, player_num):
+        self.callback(player_num, self.players[player_num], self.current_player, self.states)
+        
+    def _dispatch_all(self):
+        for i in range(self.player_num):
+            self._dispatch_callback(i)
         
     def draw(self, player_num):
         if player_num != self.current_player:
-            raise Exception()
+            return #raise Exception()
         card = self._get_card()
         self.players[player_num].append(card)
         self.reeval_state(player_num)
-        self.ignore(player_num)
-        return card
+        self._next()
+        self._dispatch_all()
     
-    def ignore(self, player_num):
+    def _next(self):
         self.current_player += 1
-        while self.states[self.current_player] == PlayerState.busted or self.current_player == self.num_players:
+        while self.states[self.current_player] == PlayerState.busted or \
+              self.current_player == self.num_players:
             if self.current_player == self.num_players:
                 self.dealer_move()
                 self.current_player = 0
             else:
                 self.current_player += 1
-                
+    
+    def ignore(self, player_num):
+        if player_num != self.current_player:
+            return #raise Exception()
+        self._next()
+        self._dispatch_all()
+        
     def dealer_move(self):
         if get_hand_value(self.dealer) < dealer_bound:
             self.dealer.append(self._get_card())
@@ -57,7 +78,9 @@ class BlackjackGame:
     def get_current_player():
         return self.current_player
     
-    def get_state(self, player_num):
+    def get_state(self, player_num=None):
+        if player_num is None:
+            return self.game_state
         return self.states[player_num]
 
     def _reeval_state(self, player_num):
@@ -66,6 +89,5 @@ class BlackjackGame:
             self.states[player_num] = PlayerState.blackjack
         elif player_value > blackjack:
             self.states[player_num] = PlayerState.busted
-            
-def get_hand_value(hand):
-    return sum(map(get_card_value, hand))
+            if player_num == self.num_players:
+                self.game_state = GameState.dealer_busted
--- main.py
+++ main.py
@@ -3,61 +3,38 @@ from tkinter import messagebox
 
 from blackjack import BlackjackGame
 
-def refresh(game, user_cards_var, dealer_cards_var):
-    user_cards_var.set("Your cards: {0}".format(get_user_cards(game)))
-    dealer_cards_var.set("Dealer cards: {0}".format(get_dealer_cards(game)))
 
-def check_state(game):
-    global root
-    if game.get_state() == GameState.ongoing:
-        return
-    else:
-        messagebox.showinfo(game.get_state().name, "Lol")
-        root.destroy()
-
-def pass_onclick(game, user_cards_var, dealer_cards_var):
-    game.ignore()
-    refresh(game, user_cards_var, dealer_cards_var)
-    check_state(game)
-
-def draw_onclick(game, user_cards_var, dealer_cards_var):
-    game.draw()
-    refresh(game, user_cards_var, dealer_cards_var)
-    check_state(game)
-
-def get_user_cards(game):
-    cards = game.get_player_hand()
-    return ' '.join(map(str, cards))
-
-def get_dealer_cards(game):
-    cards = game.get_dealer_hand()
-    return ' '.join(map(str, cards))
+def callback(player_num, player_hand, current, states):
+    user_cards_vars[player_num].set("Your cards: {0}".format(' '.join(map(str, cards))))
 
 root = tk.Tk()
 
-root.geometry("800x600")
+num_players = 3
+game = BlackjackGame(num_players, callback)
+user_cards_vars = [None for i in range(num_players)]
 
-pass_btn = tk.Button(root, text="PASS", width=10, height=5, bg="lightgrey", fg="black")
-draw_btn = tk.Button(root, text="DRAW", width=10, height=5, bg="lightgrey", fg="black")
 
-pass_btn.bind("<Button>", lambda event: pass_onclick(game, user_cards_var, dealer_cards_var))
-draw_btn.bind("<Button>", lambda event: draw_onclick(game, user_cards_var, dealer_cards_var))
+for i in range(num_players):
+    window = tk.Toplevel()
+    window.geometry("300x200")
+    
+    player = tk.Label(window, text="You are player #{}".format(i))
+    player.pack()
 
-game = BlackjackGame()
+    pass_btn = tk.Button(window, text="PASS", width=10, height=3, bg="lightgrey", fg="black")
+    draw_btn = tk.Button(window, text="DRAW", width=10, height=3, bg="lightgrey", fg="black")
 
-user_cards_var = tk.StringVar()
-user_cards_var.set("Your cards: {0}".format(get_user_cards(game)))
+    pass_btn.bind("<Button>", lambda event: game.ignore(i))
+    draw_btn.bind("<Button>", lambda event: game.draw(i))
 
-dealer_cards_var = tk.StringVar()
-dealer_cards_var.set("Dealer cards: {0}".format(get_dealer_cards(game)))
+    user_cards_vars[i] = tk.StringVar()
+    user_cards_vars[i].set('Empty hand')
 
-user_cards = tk.Label(root, textvariable=user_cards_var)
-dealer_cards = tk.Label(root, textvariable=dealer_cards_var)
+    user_cards = tk.Label(window, textvariable=user_cards_vars[i])
 
-user_cards.pack()
-dealer_cards.pack()
+    user_cards.pack()
 
-pass_btn.pack()
-draw_btn.pack()
+    pass_btn.pack()
+    draw_btn.pack()
 
 root.mainloop()
