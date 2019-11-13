e4791dde3ade050c47715a77b9c63e30323adb32
Your Name <you@example.com>
2019-11-13 22:15:06 +0300

    More work

--- blackjack.py
+++ blackjack.py
@@ -26,9 +26,12 @@ class BlackjackGame:
         self.current_player = 0
         self.deck = gen_deck()
         self.states = [PlayerState.ongoing for i in range(num_players + 1)]
+        self.game_state = GameState.ongoing
         self.callback = callback
-        #self.draw() # TODO
-        #self.draw()
+        self.count_busted = 0
+        for i in range(num_players):
+            self.draw(i)
+        self._dispatch_all()
         
     def _get_card(self):
         card = self.deck[-1]
@@ -36,7 +39,7 @@ class BlackjackGame:
         return card
     
     def _dispatch_callback(self, player_num):
-        self.callback(player_num, self.hands[player_num], self.current_player, self.states)
+        self.callback(player_num, self.current_player, self.hands[player_num], self.hands[-1], self.states, self.game_state)
         
     def _dispatch_all(self):
         for i in range(self.num_players):
@@ -44,17 +47,19 @@ class BlackjackGame:
         
     def draw(self, player_num):
         if player_num != self.current_player:
-            return #raise Exception()
+            return
         card = self._get_card()
         self.hands[player_num].append(card)
         self._reeval_state(player_num)
-        self._next()
+        if self.game_state == GameState.ongoing:
+            self._next()
         self._dispatch_all()
     
     def _next(self):
         self.current_player += 1
-        while self.states[self.current_player] == PlayerState.busted or \
-              self.current_player == self.num_players:
+        while (self.states[self.current_player] == PlayerState.busted or \
+               self.current_player == self.num_players) and \
+               self.game_state == GameState.ongoing:
             if self.current_player == self.num_players:
                 self.dealer_move()
                 self.current_player = 0
@@ -63,7 +68,7 @@ class BlackjackGame:
     
     def ignore(self, player_num):
         if player_num != self.current_player:
-            return #raise Exception()
+            return
         self._next()
         self._dispatch_all()
         
@@ -71,17 +76,16 @@ class BlackjackGame:
         if get_hand_value(self.hands[-1]) < BlackjackGame.dealer_bound:
             self.hands[-1].append(self._get_card())
             self._reeval_state(self.num_players)
-    
-    def get_state(self, player_num=None):
-        if player_num is None:
-            return self.game_state
-        return self.states[player_num]
 
     def _reeval_state(self, player_num):
         player_value = get_hand_value(self.hands[player_num])
         if player_value == BlackjackGame.blackjack:
             self.states[player_num] = PlayerState.blackjack
+            self.game_state = GameState.one_winner
         elif player_value > BlackjackGame.blackjack:
             self.states[player_num] = PlayerState.busted
+            self.count_busted += 1
             if player_num == self.num_players:
                 self.game_state = GameState.dealer_busted
+            elif self.count_busted == self.num_players:
+                self.game_state = GameState.all_busted
--- main.py
+++ main.py
@@ -1,45 +1,87 @@
 import tkinter as tk
 from tkinter import messagebox
 
-from blackjack import BlackjackGame
+from blackjack import get_hand_value, BlackjackGame, GameState, PlayerState
 
-def callback(player_num, player_hand, current, states):
-    user_cards_vars[player_num].set("Your cards: {0}".format(' '.join(map(str, player_hand))))
+num_players = 3
+player_data = [dict() for i in range(num_players)]
+
+def hand_str(hand):
+    return "None" if len(hand) == 0 else ' '.join(map(str, hand))
+
+def state_string(player_num, states, game_state):
+    if game_state != GameState.ongoing:
+        reason = None
+        if game_state == GameState.dealer_busted:
+            reason = "dealer busted"
+        elif game_state == GameState.all_busted:
+            reason = "all players busted, dealer won"
+        else:
+            for i, state in enumerate(states):
+                if state == PlayerState.blackjack:
+                    if i == player_num:
+                        who = "you"
+                    else:
+                        who = "player #" + str(i + 1) if i < num_players else "dealer"
+                    reason = who + " got blackjack"
+                    break
+        return "Game is over, " + reason
+    busted_not_me = list(map(lambda p: str(p[0] + 1), filter(lambda p: p[1] == PlayerState.busted and p[0] != player_num, enumerate(states))))
+    if len(busted_not_me) > 0:
+        plural = len(busted_not_me) > 1
+        state = "Player{} {} {} busted".format("s" if plural else "", ', '.join(busted_not_me), "are" if plural else "is")
+        if states[player_num] == PlayerState.busted:
+            state += ", btw you too"
+        return state
+    else:
+        return "" if states[player_num] == PlayerState.ongoing else "You are busted"
+
+def callback(player_num, current_player, player_hand, dealer_hand, states, game_state):
+    data = player_data[player_num]
+    data["dealer_cards"].set("Dealer cards: {}, {} points".format(hand_str(dealer_hand), get_hand_value(dealer_hand)))
+    data["my_cards"].set("Your cards: {}, {} points".format(hand_str(player_hand), get_hand_value(player_hand)))
+    data["state"].set(state_string(player_num, states, game_state))
+    if game_state != GameState.ongoing:
+        data["move"].set("")
+    else:
+        data["move"].set("It's {} move".format("your" if player_num == current_player else "{} player".format(current_player + 1)))
     
-def draw(player_num):
-    return lambda e: game.draw(player_num)
-    
-def ignore(player_num):
-    return lambda e: game.ignore(player_num)
 
 root = tk.Tk()
-num_players = 3
-game = BlackjackGame(num_players, callback)
-user_cards_vars = [None for i in range(num_players)]
 
 for i in range(num_players):
     window = tk.Toplevel()
-    window.geometry("300x200")
+    window.geometry("250x200")
+    data = player_data[i]
     
-    player = tk.Label(window, text="You are player #{}".format(i))
+    player = tk.Label(window, text="You are player #{}".format(i + 1))
+    
+    data["dealer_cards"] = tk.StringVar()
+    dealer_cards = tk.Label(window, textvariable=data["dealer_cards"])
+    
+    data["my_cards"] = tk.StringVar()
+    my_cards = tk.Label(window, textvariable=data["my_cards"])
+    
+    data["state"] = tk.StringVar()
+    state = tk.Label(window, textvariable=data["state"])
+    
+    data["move"] = tk.StringVar()
+    move = tk.Label(window, textvariable=data["move"])
+    
+    pass_btn = tk.Button(window, text="PASS", width=10, height=1, bg="lightgrey", fg="black")
+    draw_btn = tk.Button(window, text="DRAW", width=10, height=1, bg="lightgrey", fg="black")
+
+    pass_btn.bind("<Button>", lambda e, i=i: game.ignore(i))
+    draw_btn.bind("<Button>", lambda e, i=i: game.draw(i))
+    
     player.pack()
-
-    pass_btn = tk.Button(window, text="PASS", width=10, height=3, bg="lightgrey", fg="black")
-    draw_btn = tk.Button(window, text="DRAW", width=10, height=3, bg="lightgrey", fg="black")
-
-    j = i
-
-    pass_btn.bind("<Button>", ignore(j))
-    draw_btn.bind("<Button>", draw(j))
-    
-    user_cards_vars[i] = tk.StringVar()
-    user_cards_vars[i].set('Empty hand')
-
-    user_cards = tk.Label(window, textvariable=user_cards_vars[i])
-
-    user_cards.pack()
-
+    dealer_cards.pack()
+    my_cards.pack()
+    state.pack()
+    move.pack()
     pass_btn.pack()
     draw_btn.pack()
+
+game = BlackjackGame(num_players, callback)
     
 root.mainloop()
