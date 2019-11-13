d467f4dc12e81c07d41c08db0f9356a7a95b3f2e
Darya Razumova <d.razumova@yandex.ru>
2019-11-13 17:33:41 +0300

    refactored main

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from tkinter import messagebox
 from blackjack import BlackjackGame
 
 def refresh(game, user_cards_var, dealer_cards_var):
-    user_cards_var.set("Your cards: {0}".format(get_user_cards(game)))
-    dealer_cards_var.set("Dealer cards: {0}".format(get_dealer_cards(game)))
+    user_cards_var.set("Your cards: {}".format(get_user_cards(game)))
+    dealer_cards_var.set("Dealer cards: {}".format(get_dealer_cards(game)))
 
 def check_state(game):
     global root
@@ -33,31 +33,30 @@ def get_dealer_cards(game):
     cards = game.get_dealer_hand()
     return ' '.join(map(str, cards))
 
-root = tk.Tk()
+def init_root(game, height, weight):
+    root = tk.Tk()
+    root.geometry("{}x{}".format(w, h))
 
-root.geometry("800x600")
+    pass_btn = tk.Button(root, text="PASS", width=10, height=5, bg="lightgrey", fg="black")
+    draw_btn = tk.Button(root, text="DRAW", width=10, height=5, bg="lightgrey", fg="black")
 
-pass_btn = tk.Button(root, text="PASS", width=10, height=5, bg="lightgrey", fg="black")
-draw_btn = tk.Button(root, text="DRAW", width=10, height=5, bg="lightgrey", fg="black")
+    pass_btn.bind("<Button>", lambda event: pass_onclick(game, user_cards_var, dealer_cards_var))
+    draw_btn.bind("<Button>", lambda event: draw_onclick(game, user_cards_var, dealer_cards_var))
 
-pass_btn.bind("<Button>", lambda event: pass_onclick(game, user_cards_var, dealer_cards_var))
-draw_btn.bind("<Button>", lambda event: draw_onclick(game, user_cards_var, dealer_cards_var))
+    user_cards_var = tk.StringVar()
+    dealer_cards_var = tk.StringVar()
+    refresh(game, user_cards_var, dealer_cards_var)
+
+    user_cards = tk.Label(root, textvariable=user_cards_var)
+    dealer_cards = tk.Label(root, textvariable=dealer_cards_var)
+
+    user_cards.pack()
+    dealer_cards.pack()
+
+    pass_btn.pack()
+    draw_btn.pack()
+
 
 game = BlackjackGame()
-
-user_cards_var = tk.StringVar()
-user_cards_var.set("Your cards: {0}".format(get_user_cards(game)))
-
-dealer_cards_var = tk.StringVar()
-dealer_cards_var.set("Dealer cards: {0}".format(get_dealer_cards(game)))
-
-user_cards = tk.Label(root, textvariable=user_cards_var)
-dealer_cards = tk.Label(root, textvariable=dealer_cards_var)
-
-user_cards.pack()
-dealer_cards.pack()
-
-pass_btn.pack()
-draw_btn.pack()
-
+root = init_root(game, 800, 600)
 root.mainloop()
