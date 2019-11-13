077cfe5e066e64da5ddb17a6a287edec91a6611c
Vladilav Yepifanov <evn1999@yandex.ru>
2019-11-13 17:43:32 +0300

    fixed/added tests for cards

--- unit.py
+++ unit.py
@@ -1,15 +1,28 @@
 import unittest
-from Card import *
+from card import *
+from blackjack import *
 
-class TestStringMethods(unittest.TestCase):
+class TestCards(unittest.TestCase):
     def setUp(self):
-        self.game = BlackjackGame()
+        # self.game = BlackjackGame()
+        pass
 
-    def test_card_values(self):
-        self.assertEqual(Card('♥', '10').get_card_value(), 10)
-        self.assertEqual(Card('♥', 'J').get_card_value(), 10)
-        self.assertEqual(Card('♥', 'A').get_card_value(), 11)
-        self.assertEqual(Card('♥', '2').get_card_value(), 2)
+    def test_card_values_10(self):
+        self.assertEqual(Card('♥', '10').value(), 10)
+    def test_card_values_J(self):
+        self.assertEqual(Card('♥', 'J').value(), 10)
+    def test_card_values_A(self):
+        self.assertEqual(Card('♥', 'A').value(), 11)
+    def test_card_values_2(self):
+        self.assertEqual(Card('♥', '2').value(), 2)
+
+    def test_card_constructor_wrong_face_throws_valueException(self):
+        with self.assertRaises(ValueError):
+            Card('♠', "face")
+
+    def test_card_constructor_wrong_suit_throws_valueException(self):
+        with self.assertRaises(ValueError):
+            Card('clubs', "A")
 
 
 if __name__ == '__main__':
