3582b6a2bcf5b04220544c713e61dbae4de07e8f
Vladilav Yepifanov <evn1999@yandex.ru>
2019-11-13 17:52:53 +0300

    added str tests, tests don't run

--- unit.py
+++ unit.py
@@ -24,6 +24,12 @@ class TestCards(unittest.TestCase):
         with self.assertRaises(ValueError):
             Card('clubs', "A")
 
+    def card_str_is_correct(self):
+        self.assertEqual('♥2', Card('♥', '2'))
+
+    def card_str_face_10_is_correct(self):
+        self.assertEqual('♥10', Card('♥', '10'))
+
 
 if __name__ == '__main__':
     unittest.main()
