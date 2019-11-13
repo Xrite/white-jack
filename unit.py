dd54d385ea9a8c2edefd5fee61e39a226298b32d
Vladilav Yepifanov <evn1999@yandex.ru>
2019-11-13 17:46:11 +0300

    removed test_ prefix from test methods names

--- unit.py
+++ unit.py
@@ -7,20 +7,20 @@ class TestCards(unittest.TestCase):
         # self.game = BlackjackGame()
         pass
 
-    def test_card_values_10(self):
+    def card_values_10(self):
         self.assertEqual(Card('♥', '10').value(), 10)
-    def test_card_values_J(self):
+    def card_values_J(self):
         self.assertEqual(Card('♥', 'J').value(), 10)
-    def test_card_values_A(self):
+    def card_values_A(self):
         self.assertEqual(Card('♥', 'A').value(), 11)
-    def test_card_values_2(self):
+    def card_values_2(self):
         self.assertEqual(Card('♥', '2').value(), 2)
 
-    def test_card_constructor_wrong_face_throws_valueException(self):
+    def card_constructor_wrong_face_throws_valueException(self):
         with self.assertRaises(ValueError):
             Card('♠', "face")
 
-    def test_card_constructor_wrong_suit_throws_valueException(self):
+    def card_constructor_wrong_suit_throws_valueException(self):
         with self.assertRaises(ValueError):
             Card('clubs', "A")
 
